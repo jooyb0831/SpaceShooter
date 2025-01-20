c1883479becd17603dd4aa78fe4c234ec021fb34
jooyb0831 <dia231@naver.com>
2025-01-20 15:49:41 +0900

    0120

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    private Transform tr;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f -> 연속적인 값 반환(부드럽게)
+        float v = Input.GetAxis("Vertical"); //cf. GetAxisRaw = -1,0,1 세가지만 반환
+
+        Debug.Log("h="+h);
+        Debug.Log("v="+v);
+
+        //Transform 컴포넌트의 position 속성값을 변경
+        tr.position += Vector3.forward * 1;
+
+        //Translate
+        tr.Translate(Vector3.forward * 1.0f);
     }
 }
