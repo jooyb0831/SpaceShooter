6682fb141258877d68eb8155a4f6dad1a269ea2b
jooyb0831 <dia231@naver.com>
2025-01-27 11:47:11 +0900

    0127

--- Assets/02.Scripts/FollowCam.cs
+++ Assets/02.Scripts/FollowCam.cs
@@ -18,6 +18,16 @@ public class FollowCam : MonoBehaviour
     [Range(0.0f, 10.0f)]
     public float height = 2.0f;
 
+    //반응 속도
+    public float damping = 10f;
+
+    //카메라 LookAt의 Offset값
+    public float targetOffset = 2.0f;
+    
+    //Smoothdamp에서 사용할 변수
+    private Vector3 velocity = Vector3.zero;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,12 +39,21 @@ public class FollowCam : MonoBehaviour
     {
         //추적해야 할 대상의 뒤쪽으로 distance만큼 이동
         //높이를 height만큼 이동
-        camTr.position = targetTr.position 
+        Vector3 pos = targetTr.position 
                         +(-targetTr.forward * distance)
                         +(Vector3.up * height);
         
+        //구면 선형 보간 함수를 사용해 부드럽게 위치를 변경
+        //camTr.position = Vector3.Slerp(camTr.position, pos, Time.deltaTime * damping); //시작위치, 목표위치, 시간
+
+        //SmoothDamp활용하여 부드럽게 변경
+        camTr.position = Vector3.SmoothDamp(camTr.position, // 시작위치
+                                            pos,            //목표위치  
+                                            ref velocity,   //현재속도
+                                            damping);       //도달시간
+                                            
         //Camera를 피벗 좌표를 향해 회전
-        camTr.LookAt(targetTr.position);
+        camTr.LookAt(targetTr.position + (targetTr.up * targetOffset));
     }
 
     // Update is called once per frame
