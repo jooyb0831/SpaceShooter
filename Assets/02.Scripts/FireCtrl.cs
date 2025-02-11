c8727a8f21881272a9b2ba4f4a0c4e71ffde59f9
jooyb0831 <dia231@naver.com>
2025-02-11 15:51:22 +0900

    0211

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//반드시 필요한 컴포넌트를 명시해해당 컴포넌트가 삭제되는 것을 방지하는 어트리뷰트
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
+
     //총알 프리팹
     public GameObject bullet;
 
     //총알 발사 좌표
     public Transform firePos;
 
+    //총소리에 사용할 오디오 음원
+    public AudioClip fireSfx;
+
+    //AUudidoSource를의 컴포넌트를 저장할 변수
+    private new AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio= GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,5 +39,6 @@ public class FireCtrl : MonoBehaviour
     {
         //Bullet 프리팹을 동적으로 생성(생성할 객체, 위치, 회전)
         Instantiate(bullet, firePos.position, firePos.rotation);
+        audio.PlayOneShot(fireSfx, 1.0f); //PlayOneSht : 한 번만재생.
     }
 }
